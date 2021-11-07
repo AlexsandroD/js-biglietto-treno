3295b376a254f44de5cf66d84b23bfaa54abc12a
Alex <adrdra88@gmail.com>
2021-11-07 21:08:56 +0100

    improved

--- js/script.js
+++ js/script.js
@@ -22,28 +22,45 @@ let total = 0;
 
 if(anni >= 18 && anni <= 65){
     total = chilometri * prezzoBigliettoKm;
+    let finalPrice = total.toFixed(2);
+    console.log(finalPrice);
     
+    document.getElementById('km').innerHTML = chilometri + ' km';
+    document.getElementById('anni').innerHTML = anni + ' anni';
+    document.getElementById('totale').innerHTML = finalPrice + ' €';
+    
+    const nero = document.querySelector('.nero');
+    nero.style.color = 'black';
 }
 
 else if(anni < 18){
     total = chilometri * prezzoBigliettoKm - (chilometri * prezzoBigliettoKm * 20 / 100);
+    let finalPrice = total.toFixed(2);
+    console.log(finalPrice);
     
+    document.getElementById('km').innerHTML = chilometri + ' km';
+    document.getElementById('anni').innerHTML = anni + ' anni';
+    document.getElementById('totale').innerHTML = finalPrice + ' € con sconto under 18';
+    
+    const nero = document.querySelector('.nero');
+    nero.style.color = 'black';
 }
 
 else{
     total = chilometri * prezzoBigliettoKm - (chilometri * prezzoBigliettoKm * 40 / 100);
+
+    let finalPrice = total.toFixed(2);
+console.log(finalPrice);
+
+document.getElementById('km').innerHTML = chilometri + ' km';
+document.getElementById('anni').innerHTML = anni + ' anni';
+document.getElementById('totale').innerHTML = finalPrice + ' € con sconto over 65';
+
+const nero = document.querySelector('.nero');
+nero.style.color = 'black';
     
 }
 
 
 
 
-let finalPrice = total.toFixed(2);
-console.log(finalPrice);
-
-document.getElementById('km').innerHTML = chilometri + ' km';
-document.getElementById('anni').innerHTML = anni + ' anni';
-document.getElementById('totale').innerHTML = finalPrice + ' €';
-
-const nero = document.querySelector('.nero');
-nero.style.color = 'black';
